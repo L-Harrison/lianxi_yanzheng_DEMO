957d3dd21c51fe52a99c6fb92e84dce7a67bc94c
L-Harrison <zlx8wan@outlook.com>
2019-06-28 08:36:02 +0800

    1.add 接口/继承等的对比测试

---
+++ ConsoleApp/InterfaceDemo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    /// <summary>
+    /// 接口测试
+    /// </summary>
+    public static class InterfaceDemo_1
+    {
+        public interface IPerson
+        {
+            void Do();
+        }
+        public class Person : IPerson
+        {
+
+            public void Do()
+            {
+                Console.WriteLine("p");
+            }
+        }
+        public class Mayor : Person
+        {
+            
+            public void Do()
+            {
+              //  base.Do();
+                Console.WriteLine("m");
+            }
+        }
+
+    }
+    public static class InterfaceDemo_2
+    {
+        public interface IPerson
+        {
+            void Do();
+        }
+        public class Person : IPerson
+        {
+
+            public virtual void Do()
+            {
+                Console.WriteLine("p");
+            }
+        }
+        public class Mayor : Person
+        {
+            public override void Do()
+            {
+                Console.WriteLine("m");
+            }
+        }
+
+    }
+
+    public static class InterfaceDemo_3
+    {
+        public interface IPerson
+        {
+            void Do();
+        }
+        public class Person : IPerson
+        {
+
+            public void Do()
+            {
+                Console.WriteLine("p");
+            }
+        }
+        public class Mayor : Person, IPerson
+        {
+            public void Do()
+            {
+                Console.WriteLine("m");
+            }
+        }
+
+    }
+
+}
--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,11 +7,35 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+
+
+            #region 接口实现demo 注意区分
+            Console.ReadLine();
+            InterfaceDemo_1.Mayor mayor = new InterfaceDemo_1.Mayor();
+            mayor.Do();
+            ((InterfaceDemo_1.Person)mayor).Do();
+            ((InterfaceDemo_1.IPerson)mayor).Do();
+
+            Console.ReadLine();
+            InterfaceDemo_2.Mayor mayor1 = new InterfaceDemo_2.Mayor();
+            mayor1.Do();
+            ((InterfaceDemo_2.Person)mayor1).Do();
+            ((InterfaceDemo_2.IPerson)mayor1).Do();
+            Console.ReadLine();
+            InterfaceDemo_3.Mayor mayor2 = new InterfaceDemo_3.Mayor();
+            mayor2.Do();
+            ((InterfaceDemo_3.Person)mayor2).Do();
+            ((InterfaceDemo_3.IPerson)mayor2).Do();
+            Console.ReadLine();
+            #endregion
+
+            #region 异步
             //进度条 
             new AsyncAwait().ProgressBar();
             //异步编程
             //new AsyncAwait().DoSomethings();
             Console.ReadLine();
-        }   
-    }       
+            #endregion
+        }
+    }
 }
